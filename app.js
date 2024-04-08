5d2bcc7c8ac34f0278c3bc36774bf29d93a9ce77
sahilavaran <sahil.avaran321@gmail.com>
2024-04-08 08:32:01 +0100

    added html file

---
+++ app.js
@@ -0,0 +1,23 @@
+const container = document.getElementById("container");
+const display = document.getElementById("display");
+
+//adding image
+const images = [
+  {
+    url: "https://images.unsplash.com/photo-1476231682828-37e571bc172f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1473448912268-2022ce9509d8?q=80&w=2041&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1470115636492-6d2b56f9146d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
+
+//creating thumbnails in function
+function createThumbnails() {
+  images.forEach(function (image) {
+    console.log(image);
+  });
+}
+createThumbnails();
